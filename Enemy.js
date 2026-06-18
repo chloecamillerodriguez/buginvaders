d95c2a11da83aa1f9ce2d06e9da50628bcc36542
ccr8935 <ccr8935@nyu.edu>
2026-06-18 10:50:26 -0400

    first commit

---
+++ Enemy.js
@@ -0,0 +1,33 @@
+export default class Enemy {
+  constructor(x, y, imageNumber) {
+    this.x = x;
+    this.y = y;
+    this.width = 14;
+    this.height = 14;
+
+    this.image = new Image();
+    this.image.src = `images/enemy.svg`;
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+
+  move(xVelocity, yVelocity) {
+    this.x += xVelocity;
+    this.y += yVelocity;
+  }
+
+  collideWith(sprite) {
+    if (
+      this.x + this.width > sprite.x &&
+      this.x < sprite.x + sprite.width &&
+      this.y + this.height > sprite.y &&
+      this.y < sprite.y + sprite.height
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
